92166021601f6a35a456bd5666a6984d89ca80cb
shomayon <shomayon@ucsc.edu>
2017-06-09 21:17:54 -0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,34 @@
+import re
+import collections
+
+
+dic = {}
+char = []
+rematch = []
+file=open('titanic.txt','r')
+count = 50
+
+
+m = file.read()
+
+match = re.findall(r'\s\s\s+\w.+', m)  #Find charactors from text
+l = len(match)
+
+for i in range(l):
+    rematch.append(match[i].strip())   #remove white spaces
+print ('rematch is: ', rematch)
+
+
+dic = {rematch.count(x):x for x in rematch}  # Building a dictionary from the list to count and rid of duplicates.
+print ('dic is:', dic)
+a, b = dic.keys(), dic.values()
+for key, item in dic.items():
+  if key < count:
+    del dic[key]
+  continue
+#od = collections.OrderedDict(sorted(dic.items()))
+#print(od)
+
+
+
+
